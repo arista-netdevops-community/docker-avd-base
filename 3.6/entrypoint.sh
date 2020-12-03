d0261c607d5a4d35abc22a94c0e410957fce36a2
Thomas Grimonet <tgrimonet@arista.com>
2020-12-03 18:48:00 +0100

    Install docker client to run docker-in-docker

--- 3.6/entrypoint.sh
+++ 3.6/entrypoint.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+DOCKER_SOCKET=/var/run/docker.sock
+DOCKER_GROUP=docker
+USER=avd
 
 # Install specific requirement file
 if [ ! -z "${AVD_REQUIREMENTS}" ]; then
   if [ -f ${AVD_REQUIREMENTS} ]; then
     echo "Install new requirements from ${AVD_REQUIREMENTS}"
-    pip3 install --user -r ${AVD_REQUIREMENTS}
+    sudo -H -u ${USER} pip3 install --upgrade --user -r ${AVD_REQUIREMENTS}
   else
     echo "Requirement file not found, skipping..."
   fi
@@ -14,7 +17,9 @@ fi
 # Install specific ANSIBLE version
 if [ ! -z "${AVD_ANSIBLE}" ]; then
     echo "Install ansible with version ${AVD_ANSIBLE}"
-    pip3 install --user ansible==${AVD_ANSIBLE}
+    # Required for migration from 2.9 to 2.10
+    sudo -H -u ${USER} pip3 uninstall -y ansible
+    sudo -H -u ${USER} pip3 install --upgrade --user ansible==${AVD_ANSIBLE}
 fi
 
 # Reconfigure AVD User id if set by user
@@ -28,6 +33,12 @@ if [ ! -z "${AVD_GID}" ]; then
   groupmod -g ${AVD_GID} avd
 fi
 
+if [ -S ${DOCKER_SOCKET} ]; then
+    sudo chmod 666 /var/run/docker.sock &>/dev/null
+fi
+
 export PATH=$PATH:/home/avd/.local/bin
+export LC_ALL=C.UTF-8
+
 cd /projects/
 su - avd -c "cd /projects && /bin/zsh"
