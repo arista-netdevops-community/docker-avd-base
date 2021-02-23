2ce13db334cae5795ac43b9c38b100c8fa70b8de
TiTom73 <tgrimonet@arista.com>
2021-02-23 20:32:00 +0100

    fix(entrypoint): Speedup startup process by unlocking UID change (#16)

--- 3.7/entrypoint.sh
+++ 3.7/entrypoint.sh
@@ -24,14 +24,25 @@ if [ ! -z "${AVD_ANSIBLE}" ]; then
 fi
 
 # Reconfigure AVD User id if set by user
-if [ ! -z "${AVD_UID}" ]; then
-  echo "Update uid for user avd with ${AVD_UID}"
+if [ ! -z "${AVD_UID}" ] && [ "${AVD_UID}" != "`id -u avd`" ] ; then
+  echo -n "Update uid for user avd with ${AVD_UID}"
+  # Command can take time as it also change permission within the python lib
   usermod -u ${AVD_UID} avd
+  # sed -i -e "s/^avd:\([^:]*\):[0-9]*:\([0-9]*\)/avd:\1:${AVD_UID}:\2/" /etc/passwd
+  echo "... updated"
+else
+  echo "skipping UID configuration"
 fi
 
-if [ ! -z "${AVD_GID}" ]; then
-  echo "Update gid for group avd with ${AVD_GID}"
-  groupmod -g ${AVD_GID} avd
+if [ -n "${AVD_GID}" ] && [ "${AVD_GID}" != "`id -g avd`" ] ; then
+  echo -n "Update gid for group avd with ${AVD_GID}"
+  # Command can take time as it also change permission within the python lib
+  usermod -u ${AVD_UID} avd
+  # sed -i -e "s/^avd:\([^:]*\):[0-9]*/avd:\1:${AVD_GID}/" /etc/group
+  # sed -i -e "s/^avd:\([^:]*\):\([0-9]*\):[0-9]*/avd:\1:\2:${AVD_GID}/" /etc/passwd
+  echo "... updated"
+else
+  echo "skipping GID configuration"
 fi
 
 # Update gitconfig with username and email
@@ -58,4 +69,4 @@ export PATH=$PATH:/home/avd/.local/bin
 export LC_ALL=C.UTF-8
 
 cd /projects/
-su - avd -c "cd /projects && /bin/zsh"
+su - avd -c "cd /projects && export ZSH_DISABLE_COMPFIX=true && /bin/zsh"
